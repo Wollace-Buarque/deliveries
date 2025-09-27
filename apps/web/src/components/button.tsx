95dc9a32a57ba46904f31bbd0c118ad7aa7113ba
Wollace Buarque <wollacebuarque@outlook.com>
2025-09-27 19:12:36 -0300

    feat: add Button component with loading state and utility function for class names

---
+++ apps/web/src/components/button.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@/lib/utils'
+import { ComponentProps } from 'react'
+import { Loading } from './loading'
+
+import colors from 'tailwindcss/colors'
+
+interface ButtonProps extends ComponentProps<'button'> {
+  isLoading?: boolean
+}
+
+export function Button({ className, children, isLoading, ...rest }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        'flex cursor-pointer items-center justify-center gap-2 rounded-md bg-sky-300 px-4 py-2 text-sm font-semibold text-sky-900 transition-colors hover:bg-sky-400',
+        className
+      )}
+      {...rest}
+      disabled={isLoading || rest.disabled}
+    >
+      {isLoading && <Loading color={colors.sky[950]} size={16} />}
+
+      {children}
+    </button>
+  )
+}
