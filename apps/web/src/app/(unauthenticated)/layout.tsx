df401e964be27468f6d2dc15d406e334e5781514
Wollace Buarque <wollacebuarque@outlook.com>
2025-09-27 19:17:05 -0300

    feat: add UnauthenticatedLayout, SignInPage, and SignUpPage pages

---
+++ apps/web/src/app/(unauthenticated)/layout.tsx
@@ -0,0 +1,16 @@
+import { IconTruckDelivery } from '@tabler/icons-react'
+import { ReactNode } from 'react'
+
+export default function UnauthenticatedLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-sky-400 flex items-center justify-center gap-2 py-3 border-b-2 border-b-sky-300">
+        <IconTruckDelivery className="text-white" />
+
+        <span className="text-white font-semibold">Deliveries</span>
+      </header>
+
+      <div className="flex flex-col flex-1 items-center justify-center max-w-7xl mx-auto">{children}</div>
+    </div>
+  )
+}
---
+++ apps/web/src/app/(unauthenticated)/register/page.tsx
@@ -0,0 +1,5 @@
+import { SignUpForm } from '@/components/signup-form/signup-form'
+
+export default function SignUpPage() {
+  return <SignUpForm />
+}
