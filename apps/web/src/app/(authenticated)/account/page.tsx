30c3fb9c6dc83efa0f92426b65deb3895a6fdc7a
Wollace Buarque <wollacebuarque@outlook.com>
2025-10-03 01:48:24 -0300

    feat: create AccountPage component with profile data fetching and update form

---
+++ apps/web/src/app/(authenticated)/account/page.tsx
@@ -0,0 +1,27 @@
+import { IconUserCog } from '@tabler/icons-react'
+
+import { Separator } from '@/components/separator'
+import { UpdateProfileForm } from '@/components/update-profile-form/update-profile-form'
+
+import { getProfileData } from '@/app/actions/user'
+
+export default async function AccountPage() {
+  const profileData = await getProfileData()
+
+  return (
+    <main className="container mx-auto p-12">
+      <div className="flex items-end gap-4">
+        <IconUserCog size={96} stroke={1} className="text-zinc-400" />
+
+        <div>
+          <h1 className="text-3xl font-semibold">Configurações</h1>
+          <p className="mt-2 text-zinc-600">Gerencie as configurações da sua conta.</p>
+        </div>
+      </div>
+
+      <Separator />
+
+      {profileData.data && <UpdateProfileForm data={profileData.data} />}
+    </main>
+  )
+}
