56e628020e1abc164d988add3f4125dd52283191
Wollace Buarque <wollacebuarque@outlook.com>
2025-10-03 00:41:41 -0300

    feat: add back button functionality to Header component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,10 +1,32 @@
-import { IconTruckDelivery } from '@tabler/icons-react'
+'use client'
+
+import { IconChevronLeft, IconTruckDelivery } from '@tabler/icons-react'
+import { usePathname, useRouter } from 'next/navigation'
+
 import { AccountDropdownMenu } from './account-dropdown-menu'
 
 export function Header() {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const shouldShowBackButton = pathname !== '/'
+
+  function handleGoBack() {
+    router.back()
+  }
+
   return (
     <header className="flex items-center justify-between border border-b-zinc-300 p-5">
-      <IconTruckDelivery />
+      <div className="flex items-center gap-1">
+        {shouldShowBackButton && (
+          <button onClick={handleGoBack} className="cursor-pointer transition-transform hover:scale-125">
+            <IconChevronLeft className="text-sky-400" />
+            <span className="sr-only">Voltar</span>
+          </button>
+        )}
+
+        <IconTruckDelivery />
+      </div>
 
       <div>
         <ul className="flex gap-4 align-baseline text-sm *:transition-colors *:hover:text-zinc-800">
